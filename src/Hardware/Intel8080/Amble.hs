be7014319fa976326e5f3cc53eba43deadc95630
Dr. ERDI Gergo <gergo@erdi.hu>
2020-09-13 11:56:07 +0800

    Simplify Haskell version of `Amble`

--- src/Hardware/Intel8080/Amble.hs
+++ src/Hardware/Intel8080/Amble.hs
@@ -10,6 +10,7 @@
 -- are collapsed.
 module Hardware.Intel8080.Amble
        ( Step(..)
+       , step
        , Amble(End), (>:>), (>++>)
        , stepsOf
        ) where
@@ -21,42 +22,47 @@ import Data.Proxy
 import Data.Singletons.TH (singletons)
 import Data.Kind (Constraint)
 
+class (SingKind a, SingKind b, SingI (MeetOf post pre)) => Meet (post :: Maybe b) (pre :: Maybe a) where
+    type MeetOf post pre :: Maybe (Either a b)
+    meetOf :: Sing post -> Sing pre -> Demote (Maybe (Either a b)) -- MeetOf post pre)
+
+instance (SingKind a, SingKind b) => Meet (Nothing :: Maybe b) (Nothing :: Maybe a) where
+    type MeetOf Nothing Nothing = Nothing
+    meetOf _ _ = Nothing
+
+instance (SingKind a, SingKind b, SingI post) => Meet (Just post :: Maybe b) (Nothing :: Maybe a) where
+    type MeetOf (Just post) Nothing = Just (Right post)
+    meetOf post _ = Right <$> fromSing post
+
+instance (SingKind a, SingKind b, SingI pre) => Meet (Nothing :: Maybe b) (Just pre :: Maybe a) where
+    type MeetOf Nothing (Just pre) = Just (Left pre)
+    meetOf _ pre = Left <$> fromSing pre
+
 data Step (pre :: a) (post :: b) t where
-    Step :: (SingI pre, SingI post) => t -> Step pre post t
+    Step :: Sing pre -> t -> Sing post -> Step pre post t
 deriving instance Functor (Step pre post)
+
+step :: (SingI pre, SingI post) => t -> Step pre post t
+step x = Step sing x sing
 
 data Ends a b
     = Empty
     | NonEmpty a b
 
+data Amble (n :: Nat) (ends :: Ends (Maybe a) (Maybe b)) t where
+    End :: Amble 0 Empty t
+    More
+        :: forall (a0 :: Maybe a) (bn :: Maybe b) n t.
+          Sing a0
+        -> t
+        -> Vec n (Demote (Maybe (Either a b)), t)
+        -> Sing bn
+        -> Amble (1 + n) (NonEmpty a0 bn) t
+deriving instance Functor (Amble n end)
+
 type family HeadC (c :: a -> Constraint) (ends :: Ends a b) :: Constraint where
     HeadC c Empty = ()
     HeadC c (NonEmpty a b) = c a
-
-type family MidC (ends1 :: Ends a b) (c :: b -> a -> Constraint) (ends2 :: Ends a b) :: Constraint where
-    MidC (NonEmpty a1 bn) c (NonEmpty an bm) = c bn an
-    MidC _ c _ = ()
-
-data Amble (ends :: Ends (Maybe a) (Maybe b)) (n :: Nat) t where
-    End :: Amble Empty 0 t
-    Snoc
-        :: (SingKind a, SingKind b, SingI a0, SingI bn)
-        => Vec n (t, Demote (Maybe (Either a b)))
-        -> t
-        -> Amble (NonEmpty a0 bn :: Ends (Maybe a) (Maybe b)) (n + 1) t
-deriving instance Functor (Amble end n)
-
-class (SingI (MeetOf post pre)) => Meet (post :: Maybe b) (pre :: Maybe a) where
-    type MeetOf post pre :: Maybe (Either a b)
-
-instance Meet Nothing Nothing where
-    type MeetOf Nothing Nothing = Nothing
-
-instance (SingI post) => Meet (Just post) Nothing where
-    type MeetOf (Just post) Nothing = Just (Right post)
-
-instance (SingI pre) => Meet Nothing (Just pre) where
-    type MeetOf Nothing (Just pre) = Just (Left pre)
 
 class (HeadC (Meet b1) ends) => Cons (a0 :: Maybe a) (b1 :: Maybe b) (ends :: Ends (Maybe a) (Maybe b)) where
     type ConsOf a0 b1 ends :: Ends (Maybe a) (Maybe b)
@@ -64,23 +70,23 @@ class (HeadC (Meet b1) ends) => Cons (a0 :: Maybe a) (b1 :: Maybe b) (ends :: En
 instance Cons a0 b1 Empty where
     type ConsOf a0 b1 Empty = NonEmpty a0 b1
 
-instance (Meet b1 a1) => Cons a0 b1 (NonEmpty a1 bm) where
+instance (Meet b1 a1, SingI a1) => Cons a0 b1 (NonEmpty a1 bm) where
     type ConsOf a0 b1 (NonEmpty a1 bm) = NonEmpty a0 bm
 
 cons
-    :: forall a b a0 b1 ends n t. (SingKind a, SingKind b, Cons a0 b1 ends)
-    => Step (a0 :: Maybe a) (b1 :: Maybe b) t
-    -> Amble ends n t
-    -> Amble (ConsOf a0 b1 ends) (n + 1) t
-cons (Step x) End = Snoc Nil x
--- cons (Step x) (Snoc xs xn :: Amble (NonEmpty r1 wn) n a) = Snoc ((x, demote @(MeetOf w r1)) :> xs) xn
-cons s@(Step x) ss@(Snoc xs xn) = Snoc ((x, after ss) :> xs) xn
-  where
-    after :: forall (a1 :: Maybe a) (bn :: Maybe b). (Cons a0 b1 (NonEmpty a1 bn)) => Amble (NonEmpty a1 bn) n t -> Demote (Maybe (Either a b))
-    after _ = demote @(MeetOf b1 a1)
+    :: forall a0 b1 ends n t. (Cons a0 b1 ends)
+    => Step a0 b1 t
+    -> Amble n ends t
+    -> Amble (1 + n) (ConsOf a0 b1 ends) t
+cons (Step a0 x b1) End = More a0 x Nil b1
+cons (Step a0 x b1) (More a1 x' xs bn) = More a0 x ((meetOf b1 a1, x') :> xs) bn
 
 infixr 5 >:>
 (>:>) = cons
+
+type family MidC (ends1 :: Ends a b) (c :: b -> a -> Constraint) (ends2 :: Ends a b) :: Constraint where
+    MidC (NonEmpty a1 bn) c (NonEmpty an bm) = c bn an
+    MidC _ c _ = ()
 
 class (MidC ends1 Meet ends2) => Append (ends1 :: Ends (Maybe a) (Maybe b)) (ends2 :: Ends (Maybe a) (Maybe b)) where
     type AppendOf ends1 ends2 :: Ends (Maybe a) (Maybe b)
@@ -94,40 +100,22 @@ instance Append ends1 Empty where
 instance (Meet bn an) => Append (NonEmpty a0 bn) (NonEmpty an bm) where
     type AppendOf (NonEmpty a0 bn) (NonEmpty an bm) = NonEmpty a0 bm
 
-append
-    :: forall rw (ends1 :: Ends (Maybe a) (Maybe b)) (ends2 :: Ends (Maybe a) (Maybe b)) n m t. (SingKind a, SingKind b, Append ends1 ends2)
-    => Amble ends1 n t
-    -> Amble ends2 m t
-    -> Amble (AppendOf ends1 ends2) (n + m) t
-append End ss' = ss'
-append ss End = ss
-append ss@(Snoc xs xn) ss'@(Snoc ys ym) = Snoc (xs ++ singleton (xn, mid ss ss') ++ ys) ym
-  where
-    mid
-        :: forall (a0 :: Maybe a) (bn :: Maybe b) (an :: Maybe a) (bm :: Maybe b). (Meet bn an)
-        => Amble (NonEmpty a0 bn) n t
-        -> Amble (NonEmpty an bm) m t
-        -> Demote (Maybe (Either a b))
-    mid _ _ = demote @(MeetOf bn an)
+append :: (Append ends1 ends2) => Amble n ends1 t -> Amble m ends2 t -> Amble (n + m) (AppendOf ends1 ends2) t
+append End ys = ys
+append (More a0 x xs bn) End = More a0 x xs bn
+append (More a0 x xs bn) (More an y ys bm) = More a0 x (xs ++ singleton (meetOf bn an, y) ++ ys) bm
 
 infixr 5 >++>
 (>++>) = append
 
 stepsOf
-    :: forall (ends :: Ends (Maybe a) (Maybe b)) n t.
-       Amble ends n t
+    :: forall a b (ends :: Ends (Maybe a) (Maybe b)) n t.
+       (SingKind a, SingKind b)
+    => Amble n ends t
     -> (Maybe (Demote a), Vec n (t, Maybe (Demote (Either a b))))
 stepsOf End = (Nothing, Nil)
-stepsOf ss@(Snoc xs xn) = (start ss, xs ++ singleton (xn, end ss))
+stepsOf (More a0 x xs bn) = (fromSing a0, go x xs)
   where
-    start
-        :: forall (a0 :: Maybe a) (bn :: Maybe b). (SingI a0)
-        => Amble (NonEmpty a0 bn) n t
-        -> Demote (Maybe a)
-    start _ = demote @a0
-
-    end
-        :: forall (a0 :: Maybe a) (bn :: Maybe b). (SingI bn)
-        => Amble (NonEmpty a0 bn) n t
-        -> Demote (Maybe (Either a b))
-    end _ = Right <$> demote @bn
+    go :: forall k. t -> Vec k (Maybe (Demote (Either a b)), t) -> Vec (1 + k) (t, Maybe (Demote (Either a b)))
+    go x Nil = singleton (x, Right <$> fromSing bn)
+    go x (Cons (ab, x') xs) = Cons (x, ab) (go x' xs)
--- src/Hardware/Intel8080/Microcode.hs
+++ src/Hardware/Intel8080/Microcode.hs
@@ -71,38 +71,38 @@ data Target
     | PC
     deriving (Show, Generic, NFDataX)
 
-type MacroSteps ends n = Amble ends n Effect
+type MacroSteps n ends = Amble n ends Effect
 
-imm1 = Step @(Just IncrPC) @Nothing ReadMem >:> End
+imm1 = step @(Just IncrPC) @Nothing ReadMem >:> End
 
 imm2 =
-    Step @(Just IncrPC) @Nothing ReadMem >:>
-    Step @Nothing       @Nothing (FromBuf AddrBuf) >:>
-    Step @(Just IncrPC) @Nothing ReadMem >:>
-    Step @Nothing       @Nothing (FromBuf AddrBuf) >:>
+    step @(Just IncrPC) @Nothing ReadMem >:>
+    step @Nothing       @Nothing (FromBuf AddrBuf) >:>
+    step @(Just IncrPC) @Nothing ReadMem >:>
+    step @Nothing       @Nothing (FromBuf AddrBuf) >:>
     End
 
 push2 =
-    Step @Nothing @(Just DecrSP) (ToBuf AddrBuf) >:>
-    Step @Nothing @(Just DecrSP) (ToBuf AddrBuf) >:>
+    step @Nothing @(Just DecrSP) (ToBuf AddrBuf) >:>
+    step @Nothing @(Just DecrSP) (ToBuf AddrBuf) >:>
     End
 
 pushPC =
-    Step @Nothing @(Just DecrSP) (ToBuf PC) >:>
-    Step @Nothing @(Just DecrSP) (ToBuf PC) >:>
+    step @Nothing @(Just DecrSP) (ToBuf PC) >:>
+    step @Nothing @(Just DecrSP) (ToBuf PC) >:>
     End
 
 pop2 =
-    Step @(Just IncrSP) @Nothing ReadMem >:>
-    Step @Nothing       @Nothing (FromBuf AddrBuf) >:>
-    Step @(Just IncrSP) @Nothing ReadMem >:>
-    Step @Nothing       @Nothing (FromBuf AddrBuf) >:>
+    step @(Just IncrSP) @Nothing ReadMem >:>
+    step @Nothing       @Nothing (FromBuf AddrBuf) >:>
+    step @(Just IncrSP) @Nothing ReadMem >:>
+    step @Nothing       @Nothing (FromBuf AddrBuf) >:>
     End
 
 shiftRotate op =
-    Step @Nothing @Nothing (Get rA) >:>
-    Step @Nothing @Nothing (Compute RegA op SetC KeepA) >:>
-    Step @Nothing @Nothing (Set rA) >:>
+    step @Nothing @Nothing (Get rA) >:>
+    step @Nothing @Nothing (Compute RegA op SetC KeepA) >:>
+    step @Nothing @Nothing (Set rA) >:>
     End
 
 type RW = Maybe (Either Addressing Addressing)
@@ -110,41 +110,41 @@ type MicroOp = (Effect, RW)
 type MicroLen = 10
 type Microcode = (Maybe Addressing, Vec MicroLen MicroOp)
 
-mc :: (KnownNat m, (n + m) ~ MicroLen) => MacroSteps _ n -> Microcode
+mc :: (KnownNat m, (n + m) ~ MicroLen) => MacroSteps n _ -> Microcode
 mc ops = let (first, ops') = stepsOf ops
          in (first, ops' ++ pure (When Nothing, Nothing))
 
 evalSrc src k = case src of
     Imm -> mc $ imm1 >++> k
-    Op (Reg r) -> mc $ Step @Nothing @Nothing (Get r) >:> k
+    Op (Reg r) -> mc $ step @Nothing @Nothing (Get r) >:> k
     Op AddrHL -> mc $
-        Step @Nothing @Nothing (Get2 rHL) >:>
-        Step @(Just Indirect) @Nothing ReadMem >:>
+        step @Nothing @Nothing (Get2 rHL) >:>
+        step @(Just Indirect) @Nothing ReadMem >:>
         k
 
 microcode :: Instr -> Microcode
 microcode NOP = mc End
 -- microcode HLT = mc _
-microcode (INT b) = mc $ Step @Nothing @Nothing (SetInt b) >:> End
+microcode (INT b) = mc $ step @Nothing @Nothing (SetInt b) >:> End
 microcode CMA = mc $
-    Step @Nothing @Nothing (Get rA) >:>
-    Step @Nothing @Nothing (Compute ConstFF SUB KeepC KeepA) >:>
-    Step @Nothing @Nothing (Set rA) >:>
+    step @Nothing @Nothing (Get rA) >:>
+    step @Nothing @Nothing (Compute ConstFF SUB KeepC KeepA) >:>
+    step @Nothing @Nothing (Set rA) >:>
     End
 microcode CMC = mc $
-    Step @Nothing @Nothing (Compute0 fC Complement0) >:>
+    step @Nothing @Nothing (Compute0 fC Complement0) >:>
     End
 microcode STC = mc $
-    Step @Nothing @Nothing (Compute0 fC ConstTrue0) >:>
+    step @Nothing @Nothing (Compute0 fC ConstTrue0) >:>
     End
 microcode (ALU fun src) = evalSrc src $
-    Step @Nothing @Nothing (Compute RegA fun SetC SetA) >:>
-    Step @Nothing @Nothing UpdateFlags >:>
-    Step @Nothing @Nothing (Set rA) >:>
+    step @Nothing @Nothing (Compute RegA fun SetC SetA) >:>
+    step @Nothing @Nothing UpdateFlags >:>
+    step @Nothing @Nothing (Set rA) >:>
     End
 microcode (CMP src) = evalSrc src $
-    Step @Nothing @Nothing (Compute RegA SUB SetC SetA) >:>
-    Step @Nothing @Nothing UpdateFlags >:>
+    step @Nothing @Nothing (Compute RegA SUB SetC SetA) >:>
+    step @Nothing @Nothing UpdateFlags >:>
     End
 microcode RRC = mc $ shiftRotate RotateR
 microcode RLC = mc $ shiftRotate RotateL
@@ -152,156 +152,156 @@ microcode RAR = mc $ shiftRotate ShiftR
 microcode RAL = mc $ shiftRotate ShiftL
 microcode (RST irq) = mc $
     pushPC >++>
-    Step @Nothing @Nothing (Rst irq) >:>
+    step @Nothing @Nothing (Rst irq) >:>
     End
 microcode JMP = mc $
     imm2 >++>
-    Step @Nothing @Nothing Jump >:>
+    step @Nothing @Nothing Jump >:>
     End
 microcode (JMPIf cond) = mc $
     imm2 >++>
-    Step @Nothing @Nothing (When $ Just cond) >:>
-    Step @Nothing @Nothing Jump >:>
+    step @Nothing @Nothing (When $ Just cond) >:>
+    step @Nothing @Nothing Jump >:>
     End
 microcode CALL = mc $
     imm2 >++>
     pushPC >++>
-    Step @Nothing @Nothing Jump >:>
+    step @Nothing @Nothing Jump >:>
     End
 microcode (CALLIf cond) = mc $
     imm2 >++>
-    Step @Nothing @Nothing (When $ Just cond) >:>
+    step @Nothing @Nothing (When $ Just cond) >:>
     pushPC >++>
-    Step @Nothing @Nothing Jump >:>
+    step @Nothing @Nothing Jump >:>
     End
 microcode RET = mc $
     pop2 >++>
-    Step @Nothing @Nothing Jump >:>
+    step @Nothing @Nothing Jump >:>
     End
 microcode (RETIf cond) = mc $
-    Step @Nothing @Nothing (When $ Just cond) >:>
+    step @Nothing @Nothing (When $ Just cond) >:>
     pop2 >++>
-    Step @Nothing @Nothing Jump >:>
+    step @Nothing @Nothing Jump >:>
     End
 microcode LDA = mc $
     imm2 >++>
-    Step @(Just Indirect) @Nothing ReadMem >:>
-    Step @Nothing         @Nothing (Set rA) >:>
+    step @(Just Indirect) @Nothing ReadMem >:>
+    step @Nothing         @Nothing (Set rA) >:>
     End
 microcode STA = mc $
     imm2 >++>
-    Step @Nothing @(Just Indirect) (Get rA) >:>
+    step @Nothing @(Just Indirect) (Get rA) >:>
     End
 microcode (LDAX rp) = mc $
-    Step @Nothing         @Nothing (Get2 rp) >:>
-    Step @(Just Indirect) @Nothing ReadMem >:>
-    Step @Nothing         @Nothing (Set rA) >:>
+    step @Nothing         @Nothing (Get2 rp) >:>
+    step @(Just Indirect) @Nothing ReadMem >:>
+    step @Nothing         @Nothing (Set rA) >:>
     End
 microcode (STAX rp) = mc $
-    Step @Nothing @Nothing         (Get2 rp) >:>
-    Step @Nothing @(Just Indirect) (Get rA) >:>
+    step @Nothing @Nothing         (Get2 rp) >:>
+    step @Nothing @(Just Indirect) (Get rA) >:>
     End
 microcode (DCX rp) = mc $
-    Step @Nothing @Nothing (Get2 rp) >:>
-    Step @Nothing @Nothing (Compute2 Dec2 KeepC) >:>
-    Step @Nothing @Nothing (Swap2 rp) >:>
+    step @Nothing @Nothing (Get2 rp) >:>
+    step @Nothing @Nothing (Compute2 Dec2 KeepC) >:>
+    step @Nothing @Nothing (Swap2 rp) >:>
     End
 microcode (INX rp) = mc $
-    Step @Nothing @Nothing (Get2 rp) >:>
-    Step @Nothing @Nothing (Compute2 Inc2 KeepC) >:>
-    Step @Nothing @Nothing (Swap2 rp) >:>
+    step @Nothing @Nothing (Get2 rp) >:>
+    step @Nothing @Nothing (Compute2 Inc2 KeepC) >:>
+    step @Nothing @Nothing (Swap2 rp) >:>
     End
 microcode (INR AddrHL) = mc $
-    Step @Nothing         @Nothing         (Get2 rHL) >:>
-    Step @(Just Indirect) @Nothing         ReadMem >:>
-    Step @Nothing         @Nothing         (Compute Const01 ADD KeepC SetA) >:>
-    Step @Nothing         @(Just Indirect) UpdateFlags >:>
+    step @Nothing         @Nothing         (Get2 rHL) >:>
+    step @(Just Indirect) @Nothing         ReadMem >:>
+    step @Nothing         @Nothing         (Compute Const01 ADD KeepC SetA) >:>
+    step @Nothing         @(Just Indirect) UpdateFlags >:>
     End
 microcode (INR (Reg r)) = mc $
-    Step @Nothing @Nothing (Get r) >:>
-    Step @Nothing @Nothing (Compute Const01 ADD KeepC SetA) >:>
-    Step @Nothing @Nothing UpdateFlags >:>
-    Step @Nothing @Nothing (Set r) >:>
+    step @Nothing @Nothing (Get r) >:>
+    step @Nothing @Nothing (Compute Const01 ADD KeepC SetA) >:>
+    step @Nothing @Nothing UpdateFlags >:>
+    step @Nothing @Nothing (Set r) >:>
     End
 microcode (DCR AddrHL) = mc $
-    Step @Nothing         @Nothing         (Get2 rHL) >:>
-    Step @(Just Indirect) @Nothing         ReadMem >:>
-    Step @Nothing         @Nothing         (Compute ConstFF ADD KeepC SetA) >:>
-    Step @Nothing         @(Just Indirect) UpdateFlags >:>
+    step @Nothing         @Nothing         (Get2 rHL) >:>
+    step @(Just Indirect) @Nothing         ReadMem >:>
+    step @Nothing         @Nothing         (Compute ConstFF ADD KeepC SetA) >:>
+    step @Nothing         @(Just Indirect) UpdateFlags >:>
     End
 microcode (DCR (Reg r)) = mc $
-    Step @Nothing @Nothing (Get r) >:>
-    Step @Nothing @Nothing (Compute ConstFF ADD KeepC SetA) >:>
-    Step @Nothing @Nothing UpdateFlags >:>
-    Step @Nothing @Nothing (Set r) >:>
+    step @Nothing @Nothing (Get r) >:>
+    step @Nothing @Nothing (Compute ConstFF ADD KeepC SetA) >:>
+    step @Nothing @Nothing UpdateFlags >:>
+    step @Nothing @Nothing (Set r) >:>
     End
 microcode (DAD rp) = mc $
-    Step @Nothing @Nothing (Get2 rp) >:>
-    Step @Nothing @Nothing (Compute2 AddHL SetC) >:>
-    Step @Nothing @Nothing (Swap2 rHL) >:>
+    step @Nothing @Nothing (Get2 rp) >:>
+    step @Nothing @Nothing (Compute2 AddHL SetC) >:>
+    step @Nothing @Nothing (Swap2 rHL) >:>
     End
 microcode DAA = mc $
-    Step @Nothing @Nothing (Get rA) >:>
-    Step @Nothing @Nothing FixupBCD >:>
-    Step @Nothing @Nothing UpdateFlags >:>
-    Step @Nothing @Nothing (Set rA) >:>
+    step @Nothing @Nothing (Get rA) >:>
+    step @Nothing @Nothing FixupBCD >:>
+    step @Nothing @Nothing UpdateFlags >:>
+    step @Nothing @Nothing (Set rA) >:>
     End
 microcode (LXI rp) = mc $
     imm2 >++>
-    Step @Nothing @Nothing (Swap2 rp) >:>
+    step @Nothing @Nothing (Swap2 rp) >:>
     End
 microcode PCHL = mc $
-    Step @Nothing @Nothing (Get2 rHL) >:>
-    Step @Nothing @Nothing Jump >:>
+    step @Nothing @Nothing (Get2 rHL) >:>
+    step @Nothing @Nothing Jump >:>
     End
 microcode SPHL = mc $
-    Step @Nothing @Nothing (Get2 rHL) >:>
-    Step @Nothing @Nothing (Swap2 SP) >:>
+    step @Nothing @Nothing (Get2 rHL) >:>
+    step @Nothing @Nothing (Swap2 SP) >:>
     End
 microcode LHLD = mc $
     imm2 >++>
-    Step @(Just Indirect) @Nothing ReadMem >:>
-    Step @Nothing         @Nothing (Set rL) >:>
-    Step @Nothing         @Nothing (Compute2 Inc2 KeepC) >:>
-    Step @(Just Indirect) @Nothing ReadMem >:>
-    Step @Nothing         @Nothing (Set rH) >:>
+    step @(Just Indirect) @Nothing ReadMem >:>
+    step @Nothing         @Nothing (Set rL) >:>
+    step @Nothing         @Nothing (Compute2 Inc2 KeepC) >:>
+    step @(Just Indirect) @Nothing ReadMem >:>
+    step @Nothing         @Nothing (Set rH) >:>
     End
 microcode SHLD = mc $
     imm2 >++>
-    Step @Nothing @(Just Indirect) (Get rL) >:>
-    Step @Nothing @Nothing         (Compute2 Inc2 KeepC) >:>
-    Step @Nothing @(Just Indirect) (Get rH) >:>
+    step @Nothing @(Just Indirect) (Get rL) >:>
+    step @Nothing @Nothing         (Compute2 Inc2 KeepC) >:>
+    step @Nothing @(Just Indirect) (Get rH) >:>
     End
 microcode XTHL = mc $
     pop2 >++>
-    Step @Nothing @Nothing (Swap2 rHL) >:>
+    step @Nothing @Nothing (Swap2 rHL) >:>
     push2
 microcode (PUSH rp) = mc $
-    Step @Nothing @Nothing (Get2 rp) >:>
+    step @Nothing @Nothing (Get2 rp) >:>
     push2
 microcode (POP rp) = mc $
     pop2 >++>
-    Step @Nothing @Nothing (Swap2 rp) >:> End
+    step @Nothing @Nothing (Swap2 rp) >:> End
 microcode (MOV (Reg r) src) = evalSrc src $
-    Step @Nothing @Nothing (Set r) >:>
+    step @Nothing @Nothing (Set r) >:>
     End
 microcode (MOV AddrHL src) = evalSrc src $
-    Step @Nothing @(Just Indirect) (Get2 rHL) >:>
+    step @Nothing @(Just Indirect) (Get2 rHL) >:>
     End
 microcode XCHG = mc $
-    Step @Nothing @Nothing (Get2 rHL) >:>
-    Step @Nothing @Nothing (Swap2 rDE) >:>
-    Step @Nothing @Nothing (Swap2 rHL) >:>
+    step @Nothing @Nothing (Get2 rHL) >:>
+    step @Nothing @Nothing (Swap2 rDE) >:>
+    step @Nothing @Nothing (Swap2 rHL) >:>
     End
 microcode IN = mc $
-    Step @(Just IncrPC) @Nothing ReadMem >:>
-    Step @Nothing       @Nothing (FromBuf AddrBuf) >:>
-    Step @(Just 'Port)  @Nothing ReadMem >:>
-    Step @Nothing       @Nothing (Set rA) >:>
+    step @(Just IncrPC) @Nothing ReadMem >:>
+    step @Nothing       @Nothing (FromBuf AddrBuf) >:>
+    step @(Just 'Port)  @Nothing ReadMem >:>
+    step @Nothing       @Nothing (Set rA) >:>
     End
 microcode OUT = mc $
-    Step @(Just IncrPC) @Nothing      ReadMem >:>
-    Step @Nothing       @Nothing      (FromBuf AddrBuf) >:>
-    Step @Nothing       @(Just 'Port) (Get rA) >:>
+    step @(Just IncrPC) @Nothing      ReadMem >:>
+    step @Nothing       @Nothing      (FromBuf AddrBuf) >:>
+    step @Nothing       @(Just 'Port) (Get rA) >:>
     End
 -- -- microcode instr = errorX $ show instr
