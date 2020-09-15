e3ac9d78023ddbce935c505be94e7d89d5d91929
Dr. ERDI Gergo <gergo@erdi.hu>
2020-09-15 12:57:25 +0800

    Rename `Meet` to `Combine`

--- src/Hardware/Intel8080/Amble.hs
+++ src/Hardware/Intel8080/Amble.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE StandaloneDeriving, DeriveFunctor #-}
 
 {-# LANGUAGE RankNTypes #-} -- Needed for `step` only
-{-# LANGUAGE UndecidableInstances #-} -- Needed for the `TypeError` instance of `Meet` only
+{-# LANGUAGE UndecidableInstances #-} -- Needed for the `TypeError` instance of `Combine` only
 
 -- | A container for sequences where each step can have a pre- and a
 -- postamble. The postamble and the preamble of two neighbouring steps
@@ -23,17 +23,17 @@ type ConflictErr post pre =
     Text "Conflict between postamble" :$$: Text "  " :<>: ShowType post :$$:
     Text "and next preamble" :$$: Text "  " :<>: ShowType pre
 
-type family Meet (post :: Maybe b) (pre :: Maybe a) :: Maybe (Either a b) where
-    Meet Nothing Nothing = Nothing
-    Meet (Just post) Nothing = Just (Right post)
-    Meet Nothing (Just pre) = Just (Left pre)
-    Meet (Just post) (Just pre) = TypeError (ConflictErr post pre)
+type family Combine (post :: Maybe b) (pre :: Maybe a) :: Maybe (Either a b) where
+    Combine Nothing Nothing = Nothing
+    Combine (Just post) Nothing = Just (Right post)
+    Combine Nothing (Just pre) = Just (Left pre)
+    Combine (Just post) (Just pre) = TypeError (ConflictErr post pre)
 
-meet
+combine
     :: forall a b (post :: Maybe b) (pre :: Maybe a).
-       (SingKind a, SingKind b, SingI (Meet post pre))
-    => Sing post -> Sing pre -> Demote (KindOf (Meet post pre))
-meet _ _ = demote @(Meet post pre)
+       (SingKind a, SingKind b, SingI (Combine post pre))
+    => Sing post -> Sing pre -> Demote (KindOf (Combine post pre))
+combine _ _ = demote @(Combine post pre)
 
 data Step (pre :: Maybe a) (post :: Maybe b) t where
     Step :: Sing pre -> t -> Sing post -> Step pre post t
@@ -59,7 +59,7 @@ deriving instance Functor (Amble n ends)
 
 type family CanCons (b1 :: Maybe b) (ends :: Ends a b) :: Constraint where
     CanCons b1 Empty = ()
-    CanCons (b1 :: Maybe b) (NonEmpty a1 bn :: Ends a b) = (SingKind a, SingKind b, SingI (Meet b1 a1))
+    CanCons (b1 :: Maybe b) (NonEmpty a1 bn :: Ends a b) = (SingKind a, SingKind b, SingI (Combine b1 a1))
 
 type family Cons (a0 :: Maybe a) (b1 :: Maybe b) (ends :: Ends a b) where
     Cons a0 b1 Empty = NonEmpty a0 b1
@@ -71,7 +71,7 @@ cons
     -> Amble n ends t
     -> Amble (1 + n) (Cons a0 b1 ends) t
 cons (Step a0 x b1) End = More a0 Nil x b1
-cons (Step a0 x b1) (More a1 xs xn bn) = More a0 ((x, meet b1 a1) :> xs) xn bn
+cons (Step a0 x b1) (More a1 xs xn bn) = More a0 ((x, combine b1 a1) :> xs) xn bn
 
 infixr 5 >:>
 (>:>) :: forall a0 b1 n ends t. (CanCons b1 ends) => Step a0 b1 t -> Amble n ends t -> Amble (1 + n) (Cons a0 b1 ends) t
@@ -89,7 +89,7 @@ type family Append (ends1 :: Ends a b) (ends2 :: Ends a b) where
 append :: (CanAppend ends1 ends2) => Amble n ends1 t -> Amble m ends2 t -> Amble (n + m) (Append ends1 ends2) t
 append End ys = ys
 append (More a0 xs xn bn) End = More a0 xs xn bn
-append (More a0 xs xn bn) (More an ys ym bm) = More a0 (xs ++ singleton (xn, meet bn an) ++ ys) ym bm
+append (More a0 xs xn bn) (More an ys ym bm) = More a0 (xs ++ singleton (xn, combine bn an) ++ ys) ym bm
 
 infixr 5 >++>
 (>++>) :: (CanAppend ends1 ends2) => Amble n ends1 t -> Amble m ends2 t -> Amble (n + m) (Append ends1 ends2) t
