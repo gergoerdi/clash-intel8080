cb96f2e3e0f3ecba54fe383bffa99a8a48ad0938
Dr. ERDI Gergo <gergo@erdi.hu>
2020-09-25 22:26:12 +0800

    Reformat `decodeInstr`

--- src/Hardware/Intel8080/Decode.hs
+++ src/Hardware/Intel8080/Decode.hs
@@ -32,85 +32,83 @@ decodeRRPP 1 1 = RAF
 decodeRRPP r p = decodeRR r p
 
 decodeInstr :: Value -> Instr
-decodeInstr b1 =
-    case b1 of
-        $(bitPattern "01110110") -> HLT
-        $(bitPattern "01......") -> MOV d s
-        $(bitPattern "00...110") -> MVI d
-        $(bitPattern "00..0001") -> LXI rr
+decodeInstr b = case b of
+    $(bitPattern "01110110") -> HLT
+    $(bitPattern "01......") -> MOV d s
+    $(bitPattern "00...110") -> MVI d
+    $(bitPattern "00..0001") -> LXI rr
 
-        $(bitPattern "00111010") -> LDA
-        $(bitPattern "00110010") -> STA
-        $(bitPattern "00101010") -> LHLD
-        $(bitPattern "00100010") -> SHLD
-        $(bitPattern "00..1010") -> LDAX rr
-        $(bitPattern "00..0010") -> STAX rr
-        $(bitPattern "11101011") -> XCHG
+    $(bitPattern "00111010") -> LDA
+    $(bitPattern "00110010") -> STA
+    $(bitPattern "00101010") -> LHLD
+    $(bitPattern "00100010") -> SHLD
+    $(bitPattern "00..1010") -> LDAX rr
+    $(bitPattern "00..0010") -> STAX rr
+    $(bitPattern "11101011") -> XCHG
 
-        $(bitPattern "10000...") -> ADD s
-        $(bitPattern "11000110") -> ADD Imm
-        $(bitPattern "10001...") -> ADC s
-        $(bitPattern "11001110") -> ADC Imm
+    $(bitPattern "10000...") -> ADD s
+    $(bitPattern "11000110") -> ADD Imm
+    $(bitPattern "10001...") -> ADC s
+    $(bitPattern "11001110") -> ADC Imm
 
-        $(bitPattern "10010...") -> SUB s
-        $(bitPattern "11010110") -> SUB Imm
-        $(bitPattern "10011...") -> SBC s
-        $(bitPattern "11011110") -> SBC Imm
+    $(bitPattern "10010...") -> SUB s
+    $(bitPattern "11010110") -> SUB Imm
+    $(bitPattern "10011...") -> SBC s
+    $(bitPattern "11011110") -> SBC Imm
 
-        $(bitPattern "10100...") -> AND s
-        $(bitPattern "11100110") -> AND Imm
+    $(bitPattern "10100...") -> AND s
+    $(bitPattern "11100110") -> AND Imm
 
-        $(bitPattern "10101...") -> XOR s
-        $(bitPattern "11101110") -> XOR Imm
+    $(bitPattern "10101...") -> XOR s
+    $(bitPattern "11101110") -> XOR Imm
 
-        $(bitPattern "10110...") -> ORA s
-        $(bitPattern "11110110") -> ORA Imm
+    $(bitPattern "10110...") -> ORA s
+    $(bitPattern "11110110") -> ORA Imm
 
-        $(bitPattern "10111...") -> CMP s
-        $(bitPattern "11111110") -> CMP Imm
+    $(bitPattern "10111...") -> CMP s
+    $(bitPattern "11111110") -> CMP Imm
 
-        $(bitPattern "00...100") -> INR d
-        $(bitPattern "00...101") -> DCR d
-        $(bitPattern "00..0011") -> INX rr
-        $(bitPattern "00..1011") -> DCX rr
+    $(bitPattern "00...100") -> INR d
+    $(bitPattern "00...101") -> DCR d
+    $(bitPattern "00..0011") -> INX rr
+    $(bitPattern "00..1011") -> DCX rr
 
-        $(bitPattern "00..1001") -> DAD rr
-        $(bitPattern "00100111") -> DAA
+    $(bitPattern "00..1001") -> DAD rr
+    $(bitPattern "00100111") -> DAA
 
-        $(bitPattern "00000111") -> RLC
-        $(bitPattern "00001111") -> RRC
-        $(bitPattern "00010111") -> RAL
-        $(bitPattern "00011111") -> RAR
+    $(bitPattern "00000111") -> RLC
+    $(bitPattern "00001111") -> RRC
+    $(bitPattern "00010111") -> RAL
+    $(bitPattern "00011111") -> RAR
 
-        $(bitPattern "00101111") -> CMA
-        $(bitPattern "00111111") -> CMC
-        $(bitPattern "00110111") -> STC
+    $(bitPattern "00101111") -> CMA
+    $(bitPattern "00111111") -> CMC
+    $(bitPattern "00110111") -> STC
 
-        $(bitPattern "11000011") -> JMP
-        $(bitPattern "11...010") -> JMPIf cond
-        $(bitPattern "11001101") -> CALL
-        $(bitPattern "11...100") -> CALLIf cond
-        $(bitPattern "11001001") -> RET
-        $(bitPattern "11...000") -> RETIf cond
+    $(bitPattern "11000011") -> JMP
+    $(bitPattern "11...010") -> JMPIf cond
+    $(bitPattern "11001101") -> CALL
+    $(bitPattern "11...100") -> CALLIf cond
+    $(bitPattern "11001001") -> RET
+    $(bitPattern "11...000") -> RETIf cond
 
-        $(bitPattern "11011011") -> IN
-        $(bitPattern "11010011") -> OUT
+    $(bitPattern "11011011") -> IN
+    $(bitPattern "11010011") -> OUT
 
-        $(bitPattern "11101001") -> PCHL
-        $(bitPattern "11..0101") -> PUSH rrpp
-        $(bitPattern "11..0001") -> POP rrpp
-        $(bitPattern "11100011") -> XTHL
-        $(bitPattern "11111001") -> SPHL
+    $(bitPattern "11101001") -> PCHL
+    $(bitPattern "11..0101") -> PUSH rrpp
+    $(bitPattern "11..0001") -> POP rrpp
+    $(bitPattern "11100011") -> XTHL
+    $(bitPattern "11111001") -> SPHL
 
-        $(bitPattern "11111011") -> EI
-        $(bitPattern "11110011") -> DI
-        $(bitPattern "11...111") -> RST rst
-        $(bitPattern "00000000") -> NOP
-        -- _ -> error $ printf "Unknown opcode: %02x" (fromIntegral b1 :: Word8)
-        _ -> NOP
-
+    $(bitPattern "11111011") -> EI
+    $(bitPattern "11110011") -> DI
+    $(bitPattern "11...111") -> RST rst
+    $(bitPattern "00000000") -> NOP
+    -- _ -> error $ printf "Unknown opcode: %02x" (fromIntegral b1 :: Word8)
+    _ -> NOP
   where
-    b1'@(_ :> _ :> d2@r :> d1@p :> d0 :> s2 :> s1 :> s0 :> Nil) = bitCoerce b1 :: Vec 8 Bit
+    b'@(_ :> _ :> d2@r :> d1@p :> d0 :> s2 :> s1 :> s0 :> Nil) = bitCoerce b :: Vec 8 Bit
     d = decodeLHS $ bitCoerce (d2, d1, d0)
     s = LHS $ decodeLHS $ bitCoerce (s2, s1, s0)
     rst = bitCoerce (d2, d1, d0)
