3f5375ec36a49baef092f1cea15429a60f0fe84a
Dr. ERDI Gergo <gergo@erdi.hu>
2020-09-26 10:30:33 +0800

    Use `slice` in instruction decoding

--- src/Hardware/Intel8080/Decode.hs
+++ src/Hardware/Intel8080/Decode.hs
@@ -7,35 +7,35 @@ import Clash.Prelude
 import Hardware.Intel8080
 import Debug.Trace
 
-decodeLHS :: Reg -> LHS
-decodeLHS 6 = Addr RHL
-decodeLHS reg = Reg reg
+decodeLHS :: BitVector 3 -> LHS
+decodeLHS 0b110 = Addr RHL
+decodeLHS reg   = Reg (bitCoerce reg)
 
-decodeCond :: Vec 3 Bit -> Cond
+decodeCond :: BitVector 3 -> Cond
 decodeCond cond = Cond flag b
   where
-    (flag0, b) = unpack (pack cond) :: (Unsigned 2, Bool)
+    (flag0, b) = bitCoerce cond :: (BitVector 2, Bool)
     flag = case flag0 of
         0b00 -> FZ
         0b01 -> FC
         0b10 -> FP
         0b11 -> FS
 
-decodeRR :: Bit -> Bit -> RegPair
-decodeRR 0 0 = RBC
-decodeRR 0 1 = RDE
-decodeRR 1 0 = RHL
-decodeRR 1 1 = SP
+decodeRR :: BitVector 2 -> RegPair
+decodeRR 0b00 = RBC
+decodeRR 0b01 = RDE
+decodeRR 0b10 = RHL
+decodeRR 0b11 = SP
 
-decodeRRPP :: Bit -> Bit -> RegPair
-decodeRRPP 1 1 = RAF
-decodeRRPP r p = decodeRR r p
+pushPopRR :: RegPair -> RegPair
+pushPopRR SP = RAF
+pushPopRR rr = rr
 
 decodeInstr :: Value -> Instr
 decodeInstr b = case b of
     $(bitPattern "01110110") -> HLT
-    $(bitPattern "01......") -> MOV d s
-    $(bitPattern "00...110") -> MVI d
+    $(bitPattern "01......") -> MOV dest src
+    $(bitPattern "00...110") -> MVI dest
     $(bitPattern "00..0001") -> LXI rr
 
     $(bitPattern "00111010") -> LDA
@@ -46,30 +46,30 @@ decodeInstr b = case b of
     $(bitPattern "00..0010") -> STAX rr
     $(bitPattern "11101011") -> XCHG
 
-    $(bitPattern "10000...") -> ADD s
+    $(bitPattern "10000...") -> ADD src
     $(bitPattern "11000110") -> ADD Imm
-    $(bitPattern "10001...") -> ADC s
+    $(bitPattern "10001...") -> ADC src
     $(bitPattern "11001110") -> ADC Imm
 
-    $(bitPattern "10010...") -> SUB s
+    $(bitPattern "10010...") -> SUB src
     $(bitPattern "11010110") -> SUB Imm
-    $(bitPattern "10011...") -> SBC s
+    $(bitPattern "10011...") -> SBC src
     $(bitPattern "11011110") -> SBC Imm
 
-    $(bitPattern "10100...") -> AND s
+    $(bitPattern "10100...") -> AND src
     $(bitPattern "11100110") -> AND Imm
 
-    $(bitPattern "10101...") -> XOR s
+    $(bitPattern "10101...") -> XOR src
     $(bitPattern "11101110") -> XOR Imm
 
-    $(bitPattern "10110...") -> ORA s
+    $(bitPattern "10110...") -> ORA src
     $(bitPattern "11110110") -> ORA Imm
 
-    $(bitPattern "10111...") -> CMP s
+    $(bitPattern "10111...") -> CMP src
     $(bitPattern "11111110") -> CMP Imm
 
-    $(bitPattern "00...100") -> INR d
-    $(bitPattern "00...101") -> DCR d
+    $(bitPattern "00...100") -> INR dest
+    $(bitPattern "00...101") -> DCR dest
     $(bitPattern "00..0011") -> INX rr
     $(bitPattern "00..1011") -> DCX rr
 
@@ -96,22 +96,24 @@ decodeInstr b = case b of
     $(bitPattern "11010011") -> OUT
 
     $(bitPattern "11101001") -> PCHL
-    $(bitPattern "11..0101") -> PUSH rrpp
-    $(bitPattern "11..0001") -> POP rrpp
+    $(bitPattern "11..0101") -> PUSH rr'
+    $(bitPattern "11..0001") -> POP rr'
     $(bitPattern "11100011") -> XTHL
     $(bitPattern "11111001") -> SPHL
 
     $(bitPattern "11111011") -> EI
     $(bitPattern "11110011") -> DI
-    $(bitPattern "11...111") -> RST rst
+    $(bitPattern "11...111") -> RST (bitCoerce op1)
     $(bitPattern "00000000") -> NOP
     -- _ -> error $ printf "Unknown opcode: %02x" (fromIntegral b1 :: Word8)
     _ -> NOP
   where
-    b'@(_ :> _ :> d2@r :> d1@p :> d0 :> s2 :> s1 :> s0 :> Nil) = bitCoerce b :: Vec 8 Bit
-    d = decodeLHS $ bitCoerce (d2, d1, d0)
-    s = LHS $ decodeLHS $ bitCoerce (s2, s1, s0)
-    rst = bitCoerce (d2, d1, d0)
-    rr = decodeRR r p
-    rrpp = decodeRRPP r p
-    cond = decodeCond (d2 :> d1 :> d0 :> Nil)
+    op1 = slice (SNat @5) (SNat @3) b
+    op2 = slice (SNat @2) (SNat @0) b
+
+    dest = decodeLHS op1
+    src = LHS $ decodeLHS op2
+
+    rr = decodeRR $ slice (SNat @5) (SNat @4) b
+    rr' = pushPopRR rr
+    cond = decodeCond op1
