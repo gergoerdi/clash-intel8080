c76539791f31cc43664d4ba4c259b703c897b8be
Dr. ERDI Gergo <gergo@erdi.hu>
2020-10-12 20:08:39 +0800

    Fiddle with `Steps` to make it book-ready

--- src/Hardware/Intel8080/Steps.hs
+++ src/Hardware/Intel8080/Steps.hs
@@ -14,6 +14,10 @@ data IMaybe (isJust :: Bool) a where
     IJust :: a -> IMaybe True a
 deriving instance (Show a) => Show (IMaybe isJust a)
 
+fromIMaybe :: IMaybe free a -> Maybe a
+fromIMaybe INothing = Nothing
+fromIMaybe (IJust x) = Just x
+
 class Impossible where
     impossible :: a
 
@@ -23,10 +27,15 @@ type family Compat post1 pre2 where
 
 data Step pre a post hasPre hasPost where
     Step :: IMaybe hasPre pre -> a -> IMaybe hasPost post -> Step pre a post hasPre hasPost
+deriving instance (Show a, Show pre, Show post) => Show (Step pre a post hasPre hasPost)
 
 data Steps pre a post (n :: Nat) hasPre hasPost where
     One :: Step pre a post hasPre hasPost -> Steps pre a post 1 hasPre hasPost
     More :: (Compat hasPost1 hasPre2) => Step pre a post hasPre1 hasPost1 -> Steps pre a post n hasPre2 hasPost2 -> Steps pre a post (1 + n) hasPre1 hasPost2
+
+instance (Show a, Show pre, Show post) => Show (Steps pre a post hasPre n hasPost) where
+   show (One p)  = show p <> "\n"
+   show (More a b) = show a <> show b
 
 step :: IMaybe hasPre pre -> a -> IMaybe hasPost post -> Steps pre a post 1 hasPre hasPost
 step hasPre x hasPost = One $ Step hasPre x hasPost
@@ -40,23 +49,16 @@ infixr 5 >++>
 One x >++> ys = More x ys
 More x xs >++> ys = More x $ xs >++> ys
 
-deriving instance (Show a, Show pre, Show post) => Show (Step pre a post hasPre hasPost)
-instance (Show a, Show pre, Show post) => Show (Steps pre a post hasPre n hasPost) where
-   show (One p)  = show p <> "\n"
-   show (More a b) = show a <> show b
-
 stepsOf :: Steps pre a post n hasPre hasPost -> (Maybe pre, Vec n (a, Wedge post pre))
-stepsOf xs = case go xs of (hasPre, ys) -> (from hasPre, ys)
+stepsOf xs = case go xs of (hasPre, ys) -> (fromIMaybe hasPre, ys)
   where
     go :: Steps pre a post n hasPre hasPost -> (IMaybe hasPre pre, Vec n (a, Wedge post pre))
-    go (One (Step hasPre x hasPost)) = (hasPre, singleton (x, wedgeLeft $ from hasPost))
-    go (More (Step hasPre x hasPost) xs) = case go xs of (hasPre', ys) -> (hasPre, (x, combine hasPost hasPre') :> ys)
-
-    combine :: (Compat hasPost1 hasPre2) => IMaybe hasPost1 post -> IMaybe hasPre2 pre -> Wedge post pre
-    combine INothing pre    = wedgeRight $ from pre
-    combine post INothing   = wedgeLeft $ from post
-    combine IJust{} IJust{} = impossible
-
-    from :: IMaybe free m -> Maybe m
-    from INothing = Nothing
-    from (IJust x) = Just x
+    go (One (Step pre x post)) =
+        (pre, singleton (x, wedgeLeft $ fromIMaybe post))
+    go (More (Step pre x post) xs) =
+        let (pre', ys) = go xs
+            combined = case (post, pre') of
+                (INothing, pre) -> wedgeRight $ fromIMaybe pre
+                (post, INothing) -> wedgeLeft $ fromIMaybe post
+                (IJust{}, IJust{}) -> impossible
+        in (pre, (x, combined) :> ys)
