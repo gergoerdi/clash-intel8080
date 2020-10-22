62a457d941d41f93a2ef3c795cf6718fe8350d31
Dr. ERDI Gergo <gergo@erdi.hu>
2020-10-22 18:41:35 +0800

    Share more code between `addC`/`subC`/`bcd`

--- src/Hardware/Intel8080/ALU.hs
+++ src/Hardware/Intel8080/ALU.hs
@@ -12,35 +12,32 @@ byte :: (Unsigned 4, Unsigned 4) -> Value
 byte = bitCoerce
 
 binALU :: ALU -> Value -> (Bool, Bool, Value) -> (Bool, Bool, Value)
-binALU fun x (ac, c, y) = case fun of
-    Add c0 -> addC (c0 && c) x y
-    Sub c0 -> subC (c0 && c) x y
+binALU fun x (a, c, y) = case fun of
+    Add c0 -> addC (c0 && c)
+    Sub c0 -> subC (c0 && c)
     And    -> (testBit (x .|. y) 4, False, x .&. y)
     Or     -> (False, False, x .|. y)
     XOr    -> (False, False, x `xor` y)
-    BCD    -> bcd ac c y
+    BCD    -> bcd
   where
-    addC c x y = (a, c', byte (zh, zl))
-      where
-        (xh, xl) = nybbles x
-        (yh, yl) = nybbles y
-        (a, zl) = bitCoerce $ (xl `add` yl) + if c then 1 else 0
-        (c', zh) = bitCoerce $ (xh `add` yh) + if a then 1 else 0
+    (xh, xl) = nybbles x
+    (yh, yl) = nybbles y
 
-    subC c x y = (a, c', byte (zh, zl))
+    addC c = (a', c', byte (zh, zl))
       where
-        (xh, xl) = nybbles x
-        (yh, yl) = nybbles y
-        (a, zl) = bitCoerce $ (xl `sub` yl) - if c then 1 else 0
-        (c', zh) = bitCoerce $ (xh `sub` yh) - if a then 1 else 0
+        (a', zl) = bitCoerce $ (xl `add` yl) + if c then 1 else 0
+        (c', zh) = bitCoerce $ (xh `add` yh) + if a' then 1 else 0
 
-    bcd a c y = (ac', c', z)
+    subC c = (a', c', byte (zh, zl))
       where
-        (_, y0) = nybbles y
-        (ac', y') = bitCoerce $ add y $ if y0 > 9 || ac then (0x06 :: Value) else 0
+        (a', zl) = bitCoerce $ (xl `sub` yl) - if c then 1 else 0
+        (c', zh) = bitCoerce $ (xh `sub` yh) - if a' then 1 else 0
 
-        (y1, _) = nybbles y'
-        (c', z) = bitCoerce $ add y' $ if y1 > 9 || c then (0x60 :: Value) else 0
+    bcd = (a', c', z)
+      where
+        (a', w) = bitCoerce $ add y $ if yl > 9 || a then (0x06 :: Value) else 0
+        (wh, _) = nybbles w
+        (c', z) = bitCoerce $ add w $ if wh > 9 || c then (0x60 :: Value) else 0
 
 shiftRotateALU :: Either ShiftRotate ShiftRotate -> (Bool, Value) -> (Bool, Value)
 shiftRotateALU fun (c, x) = case fun of
